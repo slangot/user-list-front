a4ce51f6cddafb8fe93b9e9e724b2ef51d82f9be
Shawn <shawn17@live.fr>
2022-02-17 23:33:47 +0100

    Destructuring props and displaying the user data into a card

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,8 +1,21 @@
 import React from "react";
 
-const Card = (props) => {
-  console.log(props);
-  return <div className="Card"></div>;
+import "./Card.css";
+
+const Card = ({ user }) => {
+  console.log(user);
+  return (
+    <div className="Card">
+      <div className="first-row">
+        id: {user.id} {user.firstname} {user.lastname}
+      </div>
+      <div className="second-row">mail: {user.mail}</div>
+      <div className="buttons-column">
+        <button>&#9997;</button>
+        <button>&#10060;</button>
+      </div>
+    </div>
+  );
 };
 
 export default Card;
