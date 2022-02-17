b59c5a3ca08a6fd712af5cf95a5460a292169259
Shawn <shawn17@live.fr>
2022-02-17 23:10:41 +0100

    initial structure of Home

--- screens/Home.js
+++ screens/Home.js
@@ -1,5 +1,12 @@
+import React from "react";
+
 const Home = () => {
-  return <></>;
+  return (
+    <div className="Home">
+      <h1>Gestionnaire des utilisateurs</h1>
+      <button>Ajouter</button>
+    </div>
+  );
 };
 
 export default Home;
