784dee08daa5364f208e2827c44efbb20500ca7d
Shawn <shawn17@live.fr>
2022-02-18 15:22:11 +0100

    upgrading the user states; updating the handleUpdate with a Swal alert and confirm resume; adding an edit block to chan ge the value

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,19 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Swal from "sweetalert2";
 
 import "./Card.css";
 
-const Card = ({ user, deleteUser }) => {
+const Card = ({ user, deleteUser, updateUser }) => {
+  // Initialisation of the user data with useState
+  const [userFirstname, setUserFirstname] = useState(user.firstname);
+  const [userLastname, setUserLastname] = useState(user.lastname);
+  const [userMail, setUserMail] = useState(user.mail);
+
+  const [showUpdateBox, setShowUpdateBox] = useState(false);
+
+  const handleShowUpdateBox = () => {
+    setShowUpdateBox(!showUpdateBox);
+  };
+
+  // Function that manage the update of an user
   const handleUpdate = () => {
     Swal.fire({
-      title: "Modifier un utilisateur",
-      text: "Verifier les infos utilisateurs",
-      icon: "success",
-      confirmButtonText: "Modifier",
+      title: "Confirmer les modifications",
+      html: `<ul class="confirm-list"><li>${userFirstname}</li><li>${userLastname}</li><li>${userMail}</li></ul>`,
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "green",
+      cancelButtonColor: "red",
+      confirmButtonText: "Confirmer",
+      cancelButtonText: "Annuler",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire("Modifié", "L'utilisateur a bien été modifié", "success");
+        updateUser(user.id, userFirstname, userLastname, userMail);
+      }
     });
   };
 
+  // Function that manage the deletion of an user
   const handleDelete = () => {
     Swal.fire({
       title: "Êtes vous sûr de vouloir supprimer cette utilisateur ?",
@@ -33,18 +55,59 @@ const Card = ({ user, deleteUser }) => {
   };
 
   return (
-    <div className="Card">
-      <div className="first-row">
-        <span className="first-row-info">id: {user.id}</span>{" "}
-        <span className="first-row-info">{user.firstname}</span>{" "}
-        <span className="first-row-info">{user.lastname}</span>
+    <>
+      <div className="Card">
+        <div className="first-row">
+          <span className="first-row-info">id: {user.id}</span>
+          <span className="first-row-info">{user.firstname}</span>
+          <span className="first-row-info">{user.lastname}</span>
+        </div>
+        <div className="second-row">mail: {user.mail}</div>
+        <div className="buttons-column">
+          <button onClick={() => handleShowUpdateBox()}>&#9997;</button>
+          <button onClick={() => handleDelete()}>&#10060;</button>
+        </div>
       </div>
-      <div className="second-row">mail: {user.mail}</div>
-      <div className="buttons-column">
-        <button onClick={() => handleUpdate()}>&#9997;</button>
-        <button onClick={() => handleDelete()}>&#10060;</button>
+      <div
+        className={
+          showUpdateBox
+            ? "edit-container edit-container-show"
+            : "edit-container edit-container-hide"
+        }
+      >
+        <div className="edit-field">
+          <label htmlFor="firstname">Prénom :</label>
+          <input
+            type="text"
+            name="firstname"
+            id="firstname"
+            placeholder={user.firstname}
+            onChange={(e) => setUserFirstname(e.target.value)}
+          />
+        </div>
+        <div className="edit-field">
+          <label htmlFor="lastname">Nom :</label>
+          <input
+            type="text"
+            name="lastname"
+            id="lastname"
+            placeholder={user.lastname}
+            onChange={(e) => setUserLastname(e.target.value)}
+          />
+        </div>
+        <div className="edit-field">
+          <label htmlFor="mail">Email :</label>
+          <input
+            type="mail"
+            name="mail"
+            id="mail"
+            placeholder={user.mail}
+            onChange={(e) => setUserMail(e.target.value)}
+          />
+        </div>
+        <button onClick={() => handleUpdate()}>Modifier</button>
       </div>
-    </div>
+    </>
   );
 };
 
